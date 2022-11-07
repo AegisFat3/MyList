eb9a325f6aa1c3497b42b38a0e8d93d5370f2574
AegisFat3 <brian_cabo17@hotmail.com>
2022-11-07 11:42:57 -0600

    Primera Version

---
+++ app/src/main/java/com/example/mylist/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mylist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private List<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listViewId);
+        if(list == null)
+        {
+            if(!loadlist())
+            {
+                return;
+            }
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_detalle_list, R.id.textViewId, list);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                toast(position);
+
+            }
+        });
+    }
+    private void toast(int position){
+        Toast.makeText(getBaseContext(),list.get(position), Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean loadlist() {
+        list = new ArrayList<String>();
+        for (int i = 1; i<=100; i++)
+        {
+            list.add(String.format("Name %d", i));
+
+        }
+        return list != null && list.size() > 0;
+    }
+
+}
